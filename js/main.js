7450515c8c976ade64598179238468677a9742e4
Francisco Garcia Lopez <fran@MacBook-Pro-de-Francisco.local>
2022-05-25 21:21:06 +0200

    depuraciion codigo

--- js/main.js
+++ js/main.js
@@ -8,11 +8,6 @@ const destino = $("#destino");
 const importe = $("#importe");
 const pagar = $(".pagar");
 
-let contador = {
-  tercio: 0,
-  botellin: 0,
-};
-
 let factura = 0;
 
 const destinos = {
@@ -36,67 +31,62 @@ const destinos = {
     tercio: 2.4,
     botellin: 1.5,
   },
+  pimpam: {
+    tercio: 2.6,
+    botellin: 0,
+  },
 };
 
-sumarTercios.on("click", () => contarTercios(true));
-restarTercios.on("click", () => contarTercios(false));
-sumarBotellines.on("click", () => contarBotellines(true));
-restarBotellines.on("click", () => contarBotellines(false));
+sumarTercios.on("click", () => contarTer(true));
+restarTercios.on("click", () => contarTer(false));
+sumarBotellines.on("click", () => contarBot(true));
+restarBotellines.on("click", () => contarBot(false));
 destino.on("change", cuenta);
 pagar.on("click", borrar);
 
+getData();
 mostrar();
-cuenta();
+
+function getData() {
+  const load = JSON.parse(localStorage.getItem("contador"));
+  if (!load) {
+    const contador = {
+      tercio: 0,
+      botellin: 0,
+    };
+    localStorage.setItem("contador", JSON.stringify(contador));
+  }
+  const data = JSON.parse(localStorage.getItem("contador"));
+  return data;
+}
 
 function mostrar() {
   const caja = JSON.parse(localStorage.getItem("contador"));
-  if (!caja) {
-    return;
-  }
-  pagar.text("Pagar");
-  pagar.removeClass("no-display");
   displayTercios.text(caja.tercio);
   displayBotellines.text(caja.botellin);
 }
 
-function contarTercios(valor) {
-  if (!JSON.parse(localStorage.getItem("contador"))) {
-    localStorage.setItem("contador", JSON.stringify(contador));
-    valor ? contador.tercio++ : contador.tercio > 0 ? contador.tercio-- : null;
-    localStorage.setItem("contador", JSON.stringify(contador));
-  } else {
-    const caja = JSON.parse(localStorage.getItem("contador"));
-    valor ? caja.tercio++ : caja.tercio > 0 ? caja.tercio-- : null;
-    localStorage.setItem("contador", JSON.stringify(caja));
-  }
+function contarTer(num) {
+  const valor = getData();
+  num ? valor.tercio++ : valor.tercio > 0 ? valor.tercio-- : null;
+  localStorage.setItem("contador", JSON.stringify(valor));
+  pagar.removeClass("no-display");
+  pagar.text("Pagar");
   mostrar();
   cuenta();
 }
-function contarBotellines(valor) {
-  if (!JSON.parse(localStorage.getItem("contador"))) {
-    localStorage.setItem("contador", JSON.stringify(contador));
-    valor
-      ? contador.botellin++
-      : contador.botellin > 0
-      ? contador.botellin--
-      : null;
-    localStorage.setItem("contador", JSON.stringify(contador));
-    const { botellin } = JSON.parse(localStorage.getItem("contador"));
-    displayBotellines.text(botellin);
-  } else {
-    const caja = JSON.parse(localStorage.getItem("contador"));
-    valor ? caja.botellin++ : caja.botellin > 0 ? caja.botellin-- : null;
-    localStorage.setItem("contador", JSON.stringify(caja));
-  }
+function contarBot(num) {
+  const valor = getData();
+  num ? valor.botellin++ : valor.botellin > 0 ? valor.botellin-- : null;
+  localStorage.setItem("contador", JSON.stringify(valor));
+  pagar.removeClass("no-display");
+  pagar.text("Pagar");
   mostrar();
   cuenta();
 }
 
 function cuenta() {
   const caja = JSON.parse(localStorage.getItem("contador"));
-  if (!caja) {
-    return;
-  }
   factura =
     caja.tercio * destinos[destino.val()].tercio +
     caja.botellin * destinos[destino.val()].botellin;
@@ -104,14 +94,10 @@ function cuenta() {
 }
 
 function borrar() {
-  contador = {
-    tercio: 0,
-    botellin: 0,
-  };
-  localStorage.setItem("contador", JSON.stringify(contador));
-  mostrar();
-  cuenta();
   localStorage.removeItem("contador");
+  displayTercios.text(0);
+  displayBotellines.text(0);
+  importe.text(0);
   pagar.text("");
   pagar.addClass("no-display");
   destino.val("");
